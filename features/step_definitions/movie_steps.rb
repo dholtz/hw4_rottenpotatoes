a3ef9282a4bfdab7480bc7c032032687edd892fc
dholtz <dholtzhouser@gmail.com>
2012-03-21 14:06:39 -0500

    Add director to existing movie scenario working

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,16 @@
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+    #a_movie = Movie.new(:title => movie["title"], :rating => movie["rating"], , :director =>["director"], :release_date => movie["release_date"])
+    #a_movie.create
+    Movie.create!(movie)
+  end
+end
+
+  Then /^the director of "(.*)" should be "(.*)"$/ do |mov, dir|
+    saved_mov = Movie.find_by_title(mov)
+    puts saved_mov.director
+    assert dir.eql? saved_mov.director
+  end
+
